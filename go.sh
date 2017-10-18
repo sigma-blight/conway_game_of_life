e0abfe1f90576bd4aa7bac0c35bd35234cc7f6e4
Fletcher uni <s4428048@goliath.labs.eait.uq.edu.au>
2017-10-18 19:03:44 +1000

    MPI Implementation

--- go.sh
+++ go.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH −−partition=coursework
-#SBATCH −−job−name=yo1x4x4x1
-#SBATCH −−nodes=1
-#SBATCH −−ntasks=4
-#SBATCH −−ntasks−per−node=4
-#SBATCH −−cpus−per−task=1
+#SBATCH --partition=coursework
+#SBATCH --job-name=game_of_life
+#SBATCH --nodes=4
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:00:10
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
 DATE=$(date +"%Y%m%d%H%M")
 echo "time started  "$DATE
-echo "This is job ’$SLURM_JOB_NAME’ (id: $SLURM_JOB_ID) running on the following nodes:"
+echo "This is job '$SLURM_JOB_NAME' (id: $SLURM_JOB_ID) running on the following nodes:"
 echo $SLURM_NODELIST
 echo "running with OMP_NUM_THREADS= $OMP_NUM_THREADS "
 echo "running with SLURM_TASKS_PER_NODE= $SLURM_TASKS_PER_NODE "
@@ -18,7 +19,8 @@ echo "Now we start the show:"
 export TIMEFORMAT="%E sec"
 
 module load mpi
+#time mpirun -n ${SLURM_TASKS_PER_NODE} ./game_of_life
 time mpirun ./game -n ${SLURM_TASKS_PER_NODE}
-DATE=$(date +"%Y%m%d%H%M")
 
+DATE=$(date +"%Y%m%d%H%M")
 echo "time finished "$DATE
--- main.cpp
+++ main.cpp
@@ -6,25 +6,31 @@
 
 #include <mpi.h>
 #include <omp.h>
+MPI_Comm comm = MPI_COMM_WORLD;
 
 struct Grid
 {
+    static constexpr std::size_t cols = 50;
+
     using type_t = std::uint8_t;
     using grid_t = std::vector<type_t>;
 
-    static constexpr std::size_t size = 30;
     grid_t data;
+    const std::size_t rows;
 
-    Grid(void) : data(size * size) {}
+    Grid(const std::size_t & rows) :
+        data(rows * cols),
+        rows{ rows }
+    {}
 
     type_t & at(const std::size_t & row, const std::size_t & col)
     {
-        return data[row * size + col];
+        return data[row * cols + col];
     }
 
     const type_t & at(const std::size_t & row, const std::size_t & col) const
     {
-        return data[row * size + col];
+        return data[row * cols + col];
     }
 };
 
@@ -39,19 +45,59 @@ std::size_t get_population(const Grid &, const std::size_t &, const std::size_t 
 void init_grid(Grid &);
 void generate(Grid &, const Grid &);
 void save_grid(const Grid &, const std::size_t &);
+void print_grid(std::ofstream &, const Grid &);
 
 int main(int argc, char ** argv)
 {
-    MPI::Init(argc, argv);
-
     static constexpr std::size_t iterations = 100;
-    Grid current;
-    Grid next;
+    static constexpr int north_tag = 1;
+    static constexpr int south_tag = 2;
+
+    int rank, num_ranks;
+
+    MPI::Init(argc, argv);
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &num_ranks);
+
+    size_t rank_grid_size = std::ceil((double)Grid::cols / num_ranks);
+    if (rank == num_ranks - 1 &&
+        rank_grid_size * num_ranks >= Grid::cols) 
+        rank_grid_size -= rank_grid_size * num_ranks - Grid::cols;
+
+    if (rank > 0 && num_ranks - 1 > rank) rank_grid_size += 2;
+    else ++rank_grid_size;
+
+    Grid current{ rank_grid_size };
+    Grid next{ rank_grid_size };
     init_grid(current);
+    MPI_Request requests[2];
+    MPI_Status status[2];
+
+    std::cout << "Rank " << rank << " - Grid Size: " << rank_grid_size << "\n"; 
 
     for (std::size_t i = 0; i != iterations; ++i)
     {
-        save_grid(current, i);
+        if (rank != 0)        
+            MPI_Irecv(&current.data[0], Grid::cols, MPI::BYTE, 
+                      rank - 1, north_tag, comm, &requests[0]);        
+        
+        if (rank != num_ranks - 1)
+            MPI_Isend(&current.at(current.rows - 1, 0), Grid::cols, MPI::BYTE, 
+                      rank + 1, north_tag, comm, &requests[0]);
+
+        if (rank != num_ranks - 1)
+            MPI_Irecv(&current.at(current.rows - 1, 0), Grid::cols, MPI::BYTE,
+                      rank + 1, south_tag, comm, &requests[1]);
+
+        if (rank != 0)
+            MPI_Isend(&current.data[0], Grid::cols, MPI::BYTE,
+                      rank - 1, south_tag, comm, &requests[1]);
+
+        MPI_Wait(&requests[0], &status[0]);
+        MPI_Wait(&requests[1], &status[1]);
+        MPI_Waitall(2, requests, status);
+
+//        save_grid(current, i);
         generate(next, current);
         current.data = next.data;
     }
@@ -64,8 +110,8 @@ int main(int argc, char ** argv)
 
 void generate(Grid & next, const Grid & current)
 { 
-    for (std::size_t r = 1; r != current.size - 1; ++r)
-        for (std::size_t c = 1; c != current.size - 1; ++c)
+    for (std::size_t r = 1; r != current.rows - 1; ++r)
+        for (std::size_t c = 1; c != current.cols - 1; ++c)
         {
             const std::size_t population = get_population(current, r, c);
 
@@ -101,19 +147,71 @@ void init_grid(Grid & grid)
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(0, 1);
     
-    for (std::size_t r = 0; r != grid.size; ++r)
-        for (std::size_t c = 0; c != grid.size; ++c)
+    for (std::size_t r = 0; r != grid.rows; ++r)
+        for (std::size_t c = 0; c != grid.cols; ++c)
             grid.at(r, c) = dis(gen); 
 }
 
 void save_grid(const Grid & grid, const std::size_t & generation)
 {
-    std::stringstream ss;
-    ss << "files/save_" << generation;
-    std::ofstream file{ ss.str(), std::ios::trunc };
+    static constexpr std::size_t size_tag = 3;
+    static constexpr std::size_t data_tag = 4;
 
-    for (std::size_t r = 0; r != grid.size; ++r, file << '\n')
-        for (std::size_t c = 0; c != grid.size; ++c)
+    int rank, num_ranks;
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &num_ranks);
+
+    if (rank == 0)
+    {
+        std::stringstream ss;
+        ss << "files/save_" << generation;
+        std::ofstream file{ ss.str(), std::ios::trunc };
+
+        print_grid(file, grid);
+
+        for (int i = 1; i != num_ranks; ++i)
+        {
+            MPI_Request requests[2];
+            MPI_Status status[2];
+
+            size_t rank_rows;
+            MPI_Irecv(&rank_rows, 1, MPI::UNSIGNED, i, size_tag, 
+                MPI_COMM_WORLD, &requests[0]);
+
+            Grid data{ rank_rows };
+            MPI_Irecv(&data.data[0], Grid::cols * rank_rows,
+                    MPI::BYTE, i, data_tag, MPI_COMM_WORLD,
+                    &requests[1]);
+
+            MPI_Wait(&requests[0], &status[0]);
+            MPI_Wait(&requests[1], &status[1]);
+            MPI_Waitall(2, requests, status);
+
+            print_grid(file, data);
+        }
+    }
+    else
+    {
+        MPI_Request requests[2];
+        MPI_Status status[2];
+
+        MPI_Isend(&grid.rows, 1, MPI::UNSIGNED, 0, size_tag,
+            MPI_COMM_WORLD, &requests[0]);
+
+        MPI_Isend(&grid.data[0], grid.data.size(), MPI::BYTE, 0,
+            data_tag, MPI_COMM_WORLD, &requests[1]);
+
+ 
+        MPI_Wait(&requests[0], &status[0]);
+        MPI_Wait(&requests[1], &status[1]);
+        MPI_Waitall(2, requests, status);
+    }
+}
+
+void print_grid(std::ofstream & file, const Grid & grid)
+{
+    for (std::size_t r = 0; r != grid.rows; ++r, file << '\n')
+        for (std::size_t c = 0; c != grid.cols; ++c)
             file << (grid.at(r, c) == 0 ? '_' : 'X');
 }
 
