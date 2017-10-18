beb3b13e0dde6c1c36228a1835fad75549d9b175
Fletcher uni <s4428048@goliath.labs.eait.uq.edu.au>
2017-10-18 13:32:50 +1000

    Inital Serial Attempt - Note, seg faulting

---
+++ main.cpp
@@ -0,0 +1,201 @@
+#include <cstdlib>
+#include <vector>
+#include <cstring>
+#include <cstdio>
+#include <cstdint>
+#include <string>
+#include <fstream>
+
+class CMDData
+{
+    static void exit_invalid_arg(const char * arg, const char * msg)
+    {
+        fprintf(stderr, "Invalid CMD Arg %s - %s\n", arg, msg);
+        exit(-1);
+    }
+
+    static std::size_t get_size_t(const char * arg, const char * error_msg)
+    {
+        std::size_t res;
+        if(sscanf(arg, "%zd", &res) != 1)
+            exit_invalid_arg(arg, error_msg);
+        return res;
+    }
+
+    static std::size_t get_grid_size(const char ** argv)
+    {
+        const std::size_t res = 
+            get_size_t(argv[1], "Grid Size requires unsigned integer");
+
+        if (res < 3)
+            exit_invalid_arg(argv[1], "Grid size has to be larger than 3");
+
+        return res;
+    }
+
+    static std::size_t get_overpopulation(const char ** argv)
+    {
+        return get_size_t(argv[2], "Overpopulation requires unsigned integer");
+    }
+
+    static std::size_t get_birth(const char ** argv)
+    {
+        return get_size_t(argv[3], "Birth requires unsigned integer");
+    }
+
+    static std::size_t get_loneliness(const char ** argv)
+    {
+        return get_size_t(argv[4], "Loneliness requires unsigned integer");
+    }
+
+    static std::size_t get_iterations(const char ** argv)
+    {
+        return get_size_t(argv[5], "Iterations requires unsigned integer");
+    }
+
+public:
+
+    const std::size_t grid_size;
+    const std::size_t overpopulation;
+    const std::size_t birth;
+    const std::size_t loneliness;
+    const std::size_t iterations;
+
+    CMDData(const int argc, const char ** argv) :
+        grid_size{ get_grid_size(argv) },
+        overpopulation{ get_overpopulation(argv) },
+        birth{ get_birth(argv) },
+        loneliness{ get_loneliness(argv) },
+        iterations{ get_iterations(argv) }
+    {}        
+};
+
+struct Grid
+{
+    using type_t = std::uint8_t;
+    using grid_t = std::vector<type_t>;
+    grid_t data;
+    const std::size_t size;
+
+    Grid(const std::size_t & size) :
+        data(size),
+        size{ size }
+    {}
+
+    Grid(const Grid &) = default;
+    Grid(Grid &&) = default;
+    Grid & operator = (const Grid &) = default;
+    Grid & operator = (Grid && rhs)
+    {
+        data = std::move(rhs.data);
+        return *this;
+    }
+
+    type_t & at(const std::size_t & row, const std::size_t & col)
+    {
+        return data[row * size + col];
+    }
+
+    const type_t & at(const std::size_t & row, const std::size_t & col) const
+    {
+        return data[row * size + col];
+    }
+
+    const std::size_t & rows(void) const { return size; }
+    const std::size_t & cols(void) const { return size; }
+};
+
+class Save
+{
+    const std::size_t total_str_size;
+
+public:
+
+    Save(const std::size_t & iterations) :
+        total_str_size{ std::to_string(iterations).size() }
+    {}
+
+    void operator() (const Grid & grid, const std::size_t & progress) const
+    {
+        std::ofstream file{ std::string{ "../files/save_" }.append(
+            std::string(total_str_size - std::to_string(progress).size(), '0')).append(
+                std::to_string(progress)), std::ios::trunc};
+
+        for (std::size_t r = 0; r != grid.size; ++r)
+            for (std::size_t c = (file << '\n', 0); // new line for every row
+                             c != grid.size; ++c)
+                file << grid.at(r, c);
+
+        file.close();
+    }
+};
+
+void validate_argc(const int, const char **);
+std::size_t get_population(const Grid &, const std::size_t &, const std::size_t &);
+void generate(Grid &, const Grid &, const CMDData &);
+
+/*
+ * ./game <grid_size> <neighbourhood_type> <overpopulation> <birth> <loneliness> <iterations>
+ *
+*/
+int main(const int argc, const char ** argv)
+{
+    validate_argc(argc, argv);
+    CMDData cmd_data{ argc, argv };
+    Save save{ cmd_data.iterations };
+    Grid current{ cmd_data.grid_size };
+    Grid next{ cmd_data.grid_size };
+
+    for (std::size_t i = 0; i != cmd_data.iterations; ++i)
+    {
+        generate(next, current, cmd_data);
+//        save(next, i);
+        current = std::move(next);
+    }
+
+    return 0;
+}
+
+
+void generate(Grid & next, const Grid & current, const CMDData & cmd_data)
+{ 
+    for (std::size_t r = 1; r != current.size - r; ++r)
+        for (std::size_t c = 1; c != current.size - c; ++c)
+        {
+            const std::size_t population = get_population(current, r, c);
+
+            if (current.at(r, c) != 0 &&
+                (population > cmd_data.overpopulation ||
+                 population < cmd_data.loneliness))
+                next.at(r, c) = 0;
+            else if (current.at(r, c) == 0 &&
+                     population == cmd_data.birth)
+                next.at(r, c) = 1;
+            else
+                next.at(r, c) = current.at(r, c);
+        }
+}
+
+std::size_t get_population(const Grid & grid, 
+                           const std::size_t & row,
+                           const std::size_t & col)
+{
+    std::size_t population = 0;
+    for (std::size_t r = row - 1; r != row + 1; ++r)
+        for (std::size_t c = col - 1; c != col + 1; ++c)
+            population += grid.at(r, c);
+    return population;
+}
+
+void validate_argc(const int argc, const char ** argv)
+{
+    if (argc != 6)
+    {
+        fprintf(stderr, "Usage: %s  \
+<grid_size> <overpopulation> \
+<birth> <loneliness> <iterations>\n",
+            argv[0]);
+        exit(-1);
+    }   
+
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+game: main.cpp
+	g++ -o bin/game main.cpp -std=c++0x -O3
